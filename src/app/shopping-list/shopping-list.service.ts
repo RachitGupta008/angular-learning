fe4364167f486a4076c56b146e81336e4a02f2e0
Rachit <rachit.gupta97@yahoo.com>
2021-05-18 17:25:29 +0530

    RefactoringShopping List Component to use services

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService{
+    ingredientsAdded = new EventEmitter<Ingredient[]>();
     
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+
+    public addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsAdded.emit(this.ingredients.slice());
+    }
+
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+
 }
